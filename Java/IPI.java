60f18a91ae9ed00bf70d794f1dc47080c93f4041
LeoNardoRR <ribeiroleonardoti@gmail.com>
2023-08-28 11:12:56 -0300

    update codes

--- Java/IPI.java
+++ Java/IPI.java
@@ -1,19 +1,3 @@
-/*Escrever um algoritmo que lê:
-- a porcentagem do IPI a ser acrescido no valor das peças
-- o código da peça 1, valor unitário da peça 1, quantidade de peças 1
-- o código da peça 2, valor unitário da peça 2, quantidade de peças 2
-O algoritmo deve calcular o valor total a ser pago e apresentar o resultado.
-Fórmula : (valor1*quant1 + valor2*quant2)*(IPI/100 + 1)
-*/
-
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
-
 import java.util.Scanner;
 
 public class ipi {
---
+++ Java/salario.java
@@ -0,0 +1,11 @@
+/*Crie um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário,
+calcule a quantidade de salários mínimos esse usuário ganha e imprima o resultado.
+(1SM=R$788,00) */
+
+import java.util.Scanner;
+
+public class salario{
+    public static void main(String[] args){
+        
+    }
+}
